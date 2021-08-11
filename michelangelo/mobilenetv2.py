3f2fe8457b433e2ece10865ccdf9770195d43f73
dongTaylor <459188369@qq.com>
2021-08-11 18:25:41 +0800

    update

--- michelangelo/mobilenetv2.py
+++ michelangelo/mobilenetv2.py
@@ -1,23 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from functools import reduce
-import traceback
 
-from tensorflow.python.ops.gen_math_ops import mod
-
-def dynamic_memory_allocation():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), 'Physical GPUs,', len(logical_gpus), 'Logical GPUs')
-        except Exception:
-            traceback.print_exc()
-
-dynamic_memory_allocation()
 
 def while_loop(layer, x, training=True):
     if(isinstance(layer, list)):
